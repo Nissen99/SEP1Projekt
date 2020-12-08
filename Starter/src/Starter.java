66d882a844d5388465baf9855c6827061566352b
Antonio <32570073+Fraektsvin@users.noreply.github.com>
2020-12-08 13:53:25 +0100

    Implemented
    Interface og starter

---
+++ Starter/src/Starter.java
@@ -0,0 +1,16 @@
+/*
+import Acquaintance.IBusiness;
+import Acquaintance.IGame;
+import Business.BusinessFacade;
+import GUI.GameGUI;
+
+public class Starter {
+
+    public static void main(String[] args) {
+        IBusiness business = new BusinessFacade() {};
+        IGame game = new GameGUI();
+        game.setBusiness(business);
+        game.startApplication(args);
+    }
+}
+*/
