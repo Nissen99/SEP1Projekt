59ee422a25006dc1730a75c2b7ac02938052358a
Mikkel <Mikkelnj411@gmail.com>
2020-12-09 09:17:08 +0100

    Test om copy på TeamMemberList virker

--- LogikLag/src/TestLogik.java
+++ LogikLag/src/TestLogik.java
@@ -3,11 +3,26 @@ public class TestLogik
   public static void main(String[] args)
   {
     Employee mikkel = new Employee("Mikkel");
+    Employee mikkel2 = new Employee("Mikkel2");
+    Employee mikkel3 = new Employee("Mikkel3");
+    Employee mikkel4 = new Employee("Mikkel4");
+    Employee mikkel5 = new Employee("Mikkel5");
+
+   TeamMemberList teamMemberList = new TeamMemberList();
+    System.out.println(mikkel.getRole());
+
+    teamMemberList.addTeamMember(mikkel);
+    teamMemberList.addTeamMember(mikkel2);
+    teamMemberList.addTeamMember(mikkel3);
+    teamMemberList.addTeamMember(mikkel4);
+    teamMemberList.addTeamMember(mikkel5);
+
+    System.out.println(teamMemberList.getAllTeamMembers().get(0).getRole());
+
+    mikkel.setRole("Whatever");
 
     System.out.println(mikkel.getRole());
 
-    mikkel.copy().setRole("Whatever");
-
-    System.out.println(mikkel.getRole());
+    System.out.println(teamMemberList.getAllTeamMembers().get(0).getRole());
   }
 }
